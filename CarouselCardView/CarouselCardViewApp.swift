fef557039cf68e9a5728fda1f64e9d6e08f8b1e0
ArunKumar Chandrasekar <arunkumarchandrasekar@outlook.com>
2022-08-23 10:50:36 +0530

    Initial Commit

---
+++ CarouselCardView/CarouselCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselCardViewApp.swift
+//  CarouselCardView
+//
+//  Created by Arunkumar Chandrasekar on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
